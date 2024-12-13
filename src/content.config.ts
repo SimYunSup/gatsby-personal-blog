5472837a0af40d75d2d32ecbb473c9b010641860
Yunsup Sim <pedogunu@gmail.com>
2024-12-14 00:40:15 +0900

    fix: 주의사항 추가

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,11 +4,12 @@ import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({
-    loader: glob({ pattern: '**/*.md', base: "./src/content/posts" }),
+    loader: glob({ pattern: ['**/*.md', '**/*.mdx'], base: "./src/content/posts" }),
     schema: docsSchema({
       extend: ({ image }) => {
         return z.object({
           // Add a field that must resolve to a local image.
+          date: z.date(),
           cover: image().optional(),
         });
       },
